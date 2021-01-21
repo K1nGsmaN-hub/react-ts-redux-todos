cd0378c591f5eb7e8c6fc8c64e85f969b0c7a794
K1nGsmaN-hub <15162342h@gmail.com>
2021-01-22 03:26:33 +0800

    connect redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,25 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { applyMiddleware, compose, createStore } from 'redux';
+import { rootReducer } from './store/rootReducer';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
