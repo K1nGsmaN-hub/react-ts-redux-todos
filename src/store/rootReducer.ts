50e415b8d3a3b06b65e53af345e725dfd350449e
K1nGsmaN-hub <15162342h@gmail.com>
2021-01-23 04:14:38 +0800

    update rootReducer in src/app/store

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import { appReducer } from './reducers/appReducer';
 
-export const rootReducer = combineReducers({});
+export const rootReducer = combineReducers({
+  todos: appReducer,
+});
 
 export type RootState = ReturnType<typeof rootReducer>;
