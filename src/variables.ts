020ba9fc7c9f5f6c53b13aa457a27ce105c027c6
K1nGsmaN-hub <15162342h@gmail.com>
2021-01-23 01:18:41 +0800

    added variables in src/

---
+++ src/variables.ts
@@ -0,0 +1,29 @@
+// shadows
+export const shadow = '0px 0px 5px rgba(0, 0, 0, 0.13)';
+export const shadowActive = '0px 0px 20px rgba(0, 0, 0, 0.13)';
+export const shadowBtn = 'drop-shadow(2px 2px 10px rgba(203, 127, 127, 0.5))';
+
+// gradients
+export const lightBlueGradient90 =
+  'linear-gradient(180deg, #4481EB 0%, #04BEFE 138.79%)';
+export const purpleBlueGradient =
+  'linear-gradient(90deg, #6A11CB 0%, #2575FC 100%);';
+export const purpleGradient90 =
+  'linear-gradient(90deg, #667EEA 0%, #764BA2 100%)';
+
+export const greenGradient90 =
+  'linear-gradient(90deg, #44BB9A 9%, #8DE2CB 100%)';
+
+export const pinkGradient90 =
+  'linear-gradient(90deg, #f54ea2 13%, #ff7676 110%)';
+
+export const redGradient90 = 'linear-gradient(90deg, #ec008c 0%, #fc6767 100%)';
+export const redGradient180 =
+  'linear-gradient(180deg, #ec008c 0%, #fc6767 100%)';
+
+export const grayFade =
+  'linear-gradient(90deg, #e7e7e7 0%, rgba(0, 0, 0, 0) 88.75%)';
+
+// other
+
+export const url = 'http://localhost:3001/';
