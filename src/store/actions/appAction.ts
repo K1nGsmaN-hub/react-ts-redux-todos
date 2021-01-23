a5bdf390df30a40a689c8feb5a01eaba9a2102d2
K1nGsmaN-hub <15162342h@gmail.com>
2021-01-23 23:44:28 +0800

    added new actions in src/store/actions/appAction

--- src/store/actions/appAction.ts
+++ src/store/actions/appAction.ts
@@ -1,4 +1,9 @@
-import { ADD_TASK, DELETE_TASK } from '../types';
+import {
+  ADD_TASK,
+  DELETE_TASK,
+  DISABLE_IS_COMPLETE_STATUS,
+  ENABLE_IS_COMPLETE_STATUS,
+} from '../types';
 import { ITodo } from '../reducers/appReducer';
 
 export const addTask = (todo: ITodo) => ({
@@ -8,5 +13,14 @@ export const addTask = (todo: ITodo) => ({
 
 export const deleteTask = (id: string) => ({
   type: DELETE_TASK,
+  payload: id,
+});
+
+export const enableIsCompleteStatus = (id: string) => ({
+  type: ENABLE_IS_COMPLETE_STATUS,
+  payload: id,
+});
+export const disableIsCompleteStatus = (id: string) => ({
+  type: DISABLE_IS_COMPLETE_STATUS,
   payload: id,
 });
